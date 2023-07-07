1963c6bc90a804e3d7114d24e6e22afae12b6d14
mihee <s2809h29@gmail.com>
2023-07-07 14:07:03 +0900

    design: 마이페이지, 후기 상세페이지 화면 디자인

---
+++ src/routes/My.tsx
@@ -0,0 +1,47 @@
+import styles from '../styles/scss/my.module.scss';
+import inputstyles from '../styles/scss/auth.module.scss';
+import { useState } from 'react';
+export default function My() {
+  const [active, setActive] = useState(true);
+  const onRecommendClick = () => {
+    setActive((prev) => !prev);
+  }
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.content}>
+        <div className={styles["item-wrapper"]}>
+          <span className={styles.title}>아이디</span>
+          <span className={styles["user-info"]}>imdaxsz</span>
+          <button className={styles["btn-white"]}>로그아웃</button>
+        </div>
+        <div className={styles["item-wrapper"]}>
+          <span className={styles.title}>이메일</span>
+          <span className={styles["user-info"]}>s2809h29@gmail.com</span>
+          <button className={`${styles["btn-primary"]} ${styles.right}`}>변경</button>
+        </div>
+        <div className={styles["password-wrapper"]}>
+          <div className={styles.title}>비밀번호 재설정</div>
+          <div className={styles.password}>
+            <input className={inputstyles.input} placeholder="현재 비밀번호"></input>
+            <input className={inputstyles.input} placeholder="새 비밀번호"></input>
+            <input className={inputstyles.input} placeholder="새 비밀번호 확인"></input>
+            <button className={styles["btn-primary"]}>변경</button>
+          </div>
+        </div>
+        <div className={styles["item-wrapper"]}>
+          <span className={styles["title-md"]}>후기 작성 후 책 추천</span>
+          <div className={`toggle ${active === true ? 'toggle-active' : ''}`} onClick={onRecommendClick}>
+            <div className="circle"/>
+          </div>
+        </div>
+        <div className={styles["item-wrapper"]}>
+          <span className={styles["title-md"]}>후기 데이터 다운로드</span>
+          <button className={styles["btn-primary"]}>데이터 요청하기</button>
+        </div>
+        <div className={styles["item-wrapper"]}>
+          <a href="/#">회원 탈퇴</a>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/routes/ReviewDetail.tsx
+++ src/routes/ReviewDetail.tsx
@@ -1,7 +1,40 @@
+import { PiStarFill, PiStarLight } from "react-icons/pi";
+import styles from "../styles/scss/detail.module.scss";
 export default function ReviewDetail() {
+  const score = 3;
   return (
-    <div>
-      <div><p>흐허엉어어어</p></div>
+    <div className="wrapper">
+      <div className={styles.wrapper}>
+        <div className={styles.item}>
+          <div className={styles.tool}>
+            <div className={styles.icon}>수정</div>
+            <div className={styles.icon}>삭제</div>
+          </div>
+          <div className={styles.book}>
+            <div className={styles.thumnail}>
+              <img src="https://shopping-phinf.pstatic.net/main_3527242/35272422624.20221229074357.jpg" alt="thumnail"></img>
+            </div>
+            <div className={styles.info}>
+              <div className={styles.title}>망그러진 만화</div>
+              {/* <div className={styles.title}>미국 연방자동차안전기준 202 승객용 차량을 위한 머리지지대 제정에 대한 입법평가 사례분석</div> */}
+              <div className={styles["sub-info"]}>유랑</div>
+              <div className={styles["sub-info"]}>좋은생각</div>
+              <div className={styles.score}>
+                {[...Array(score)].map((a, i) => (
+                  <PiStarFill className="star-lg" key={i} />
+                ))}
+                {[...Array(5 - score)].map((a, i) => (
+                  <PiStarLight className="star-lg" key={i} />
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className={styles.content}>
+            <div>2023년 7월 5일 수요일</div>
+            <div>후기내용입니더 하하하하하</div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
