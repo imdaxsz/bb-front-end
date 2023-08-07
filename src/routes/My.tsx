e696741e98208cc7a16e45ffc306d75e24723ac3
mihee <s2809h29@gmail.com>
2023-08-07 23:36:40 +0900

    design: 마이페이지 아이디 영역 삭제
    - 이메일을 아이디처럼 사용할 것이기 때문에 기존 아이디 영역을 삭제함

--- src/routes/My.tsx
+++ src/routes/My.tsx
@@ -1,23 +1,21 @@
 import styles from '../styles/scss/my.module.scss';
 import inputstyles from '../styles/scss/auth.module.scss';
 import { useState } from 'react';
+
 export default function My() {
   const [active, setActive] = useState(true);
+
   const onRecommendClick = () => {
     setActive((prev) => !prev);
   }
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.content}>
         <div className={styles["item-wrapper"]}>
-          <span className={styles.title}>아이디</span>
-          <span className={styles["user-info"]}>imdaxsz</span>
-          <button className={styles["btn-white"]}>로그아웃</button>
-        </div>
-        <div className={styles["item-wrapper"]}>
           <span className={styles.title}>이메일</span>
           <span className={styles["user-info"]}>s2809h29@gmail.com</span>
-          <button className={`${styles["btn-primary"]} ${styles.right}`}>변경</button>
+          <button className={styles["btn-white"]}>로그아웃</button>
         </div>
         <div className={styles["password-wrapper"]}>
           <div className={styles.title}>비밀번호 재설정</div>
