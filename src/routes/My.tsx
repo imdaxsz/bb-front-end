2fc463a4732d5b4bc113f0fa185d1b126c042fe2
mihee <s2809h29@gmail.com>
2023-08-10 00:15:13 +0900

    feat: 마이페이지 회원 정보 출력 및 로그아웃 구현

--- src/routes/My.tsx
+++ src/routes/My.tsx
@@ -1,21 +1,49 @@
-import styles from '../styles/scss/my.module.scss';
-import inputstyles from '../styles/scss/auth.module.scss';
-import { useState } from 'react';
+import styles from "../styles/scss/my.module.scss";
+import inputstyles from "../styles/scss/auth.module.scss";
+import { useState } from "react";
+import { useEffect } from "react";
+import api from "../api/api";
 
 export default function My() {
   const [active, setActive] = useState(true);
+  const [email, setEmail] = useState('');
+  const token = localStorage.getItem("token");
 
   const onRecommendClick = () => {
     setActive((prev) => !prev);
+  };
+
+  const signOut = () => {
+    localStorage.removeItem('token');
+    window.location.href = "/";
   }
+
+  useEffect(() => {
+    if (token) {
+      api
+        .get(`/api/user/info`, {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then((res) => {
+          if (res.status === 200) {
+            setEmail(res.data.email);
+          }
+        });
+    }
+  }, [token]);
 
   return (
     <div className={styles.wrapper}>
       <div className={styles.content}>
         <div className={styles["item-wrapper"]}>
           <span className={styles.title}>이메일</span>
-          <span className={styles["user-info"]}>s2809h29@gmail.com</span>
-          <button className={styles["btn-white"]}>로그아웃</button>
+          <span className={styles["user-info"]}>{email}</span>
+          <button className={styles["btn-white"]} onClick={signOut}>
+            로그아웃
+          </button>
         </div>
         <div className={styles["password-wrapper"]}>
           <div className={styles.title}>비밀번호 재설정</div>
@@ -28,8 +56,8 @@ export default function My() {
         </div>
         <div className={styles["item-wrapper"]}>
           <span className={styles["title-md"]}>후기 작성 후 책 추천</span>
-          <div className={`toggle ${active === true ? 'toggle-active' : ''}`} onClick={onRecommendClick}>
-            <div className="circle"/>
+          <div className={`toggle ${active === true ? "toggle-active" : ""}`} onClick={onRecommendClick}>
+            <div className="circle" />
           </div>
         </div>
         <div className={styles["item-wrapper"]}>
