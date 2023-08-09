482ccc088746da9841c027d3c9960f2961503711
mihee <s2809h29@gmail.com>
2023-08-10 00:28:10 +0900

    feat: 로그인 여부에 따라 홈(후기 리스트) 페이지 수행 동작 변경
    - 로그인 상태에서만 사용자 후기 리스트 조회 api 호출
    - 로그인 상태가 아닌 경우, 로그인 유도 메시지 출력

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,51 +4,59 @@ import { Review } from "../types/types";
 import api from "../api/api";
 import { useSearchParams } from "react-router-dom";
 
-export default function Home() {
+export default function Home({ isAuthenticated }: { isAuthenticated: boolean }) {
   const [reviews, setReviews] = useState<Review[]>([]);
   const [searchParams] = useSearchParams();
   const sort = searchParams.get("sort");
+  const token = localStorage.getItem("token");
 
   useEffect(() => {
-    console.log(sort)
-    if (sort === "date_asc") {
-      api.get(`/api/review/list?sort=date_asc`).then((res) => {
-        if (res.status === 200) {
-          setReviews(res.data);
-        }
-      });
-    } else if (sort === "title") {
-      api.get(`/api/review/list?sort=title`).then((res) => {
-        if (res.status === 200) {
-          setReviews(res.data);
-        }
-      });
-    } else {
-      api.get(`/api/review/list`).then((res) => {
-        if (res.status === 200) {
-          setReviews(res.data);
-        }
-      });
+    if (isAuthenticated) {
+      let url = `/api/review/list`;
+      if (sort === "date_asc") url = `/api/review/list?sort=date_asc`;
+      else if (sort === "title") url = `/api/review/list?sort=title`;
+      try {
+        api
+          .get(url, {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`,
+            },
+          })
+          .then((res) => {
+            if (res.status === 200) {
+              setReviews(res.data);
+            }
+          });
+      } catch (error) {
+        console.log(error);
+      }
     }
-  }, [sort]);
+  }, [isAuthenticated, sort, token]);
 
   return (
     <div className="wrapper">
-      {/* <div className="guide">
-        <span>로그인 후, 나만의 책 후기를 남겨보세요!</span>
-      </div> */}
-      {reviews.length === 0 && (
+      {isAuthenticated ? (
+        <>
+          {reviews.length === 0 ? (
+            <div className="guide">
+              <span>아직 작성한 후기가 없어요.</span>
+            </div>
+          ) : (
+            <div className="list-wrapper">
+              <div className="list">
+                {reviews.map((review, i) => (
+                  <ReviewItem review={review} key={i} />
+                ))}
+              </div>
+            </div>
+          )}
+        </>
+      ) : (
         <div className="guide">
-          <span>아직 작성한 후기가 없어요.</span>
+          <span>로그인 후, 나만의 책 후기를 남겨보세요!</span>
         </div>
       )}
-      <div className="list-wrapper">
-        <div className="list">
-          {reviews.map((review, i) => (
-            <ReviewItem review={review} key={i} />
-          ))}
-        </div>
-      </div>
     </div>
   );
 }
