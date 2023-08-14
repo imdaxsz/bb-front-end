d136e41e1a6b2871eff749222084a35cd0b197e5
mihee <s2809h29@gmail.com>
2023-08-14 19:31:13 +0900

    refactor: TopBar 재사용성 향상을 위해 리팩토링
    - 페이지에 따라 TopBar 아이템 구성 및 배경색 변경
    - 후기 작성 페이지 내에 있던 커스텀 TopBar를 TopBar 컴포넌트로 변경

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,21 +1,54 @@
-import { Link } from 'react-router-dom';
-import styles from '../styles/scss/bar.module.scss';
+import { Link } from "react-router-dom";
+import styles from "../styles/scss/bar.module.scss";
 
-export default function TopBar() {
+interface Props {
+  write?: {
+    mode: "new" | "edit";
+    savedCount: number;
+    onClick: (opt: "save" | "upload") => void;
+    onNumClick: () => void;
+  };
+}
+
+export default function TopBar({ write }: Props) {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.topbar}>
+      <div className={`${styles.topbar} ${write && styles["topbar-light"]}`}>
         <ul>
           <li className={styles.logo}>
             <Link to="/">북북</Link>
           </li>
           <div className={styles.right}>
-            <li>
-              <Link to="/write?mode=new">후기작성</Link>
-            </li>
-            <li>
-              <Link to="/my">MY</Link>
-            </li>
+            {!write ? (
+              <>
+                <li>
+                  <Link to="/write?mode=new">후기작성</Link>
+                </li>
+                <li>
+                  <Link to="/my">MY</Link>
+                </li>
+              </>
+            ) : (
+              <>
+                {write.mode === "new" && (
+                  <li>
+                    <button className={styles.save}>
+                      <span onClick={() => write.onClick("save")}>저장</span>
+                      {write.savedCount > 0 && (
+                        <span onClick={write.onNumClick} className={styles.number}>
+                          {write.savedCount}
+                        </span>
+                      )}
+                    </button>
+                  </li>
+                )}
+                <li>
+                  <button className={styles.upload} onClick={() => write.onClick("upload")}>
+                    완료
+                  </button>
+                </li>
+              </>
+            )}
           </div>
         </ul>
       </div>
--- src/routes/Write.tsx
+++ src/routes/Write.tsx
@@ -1,60 +1,66 @@
 import { useRef, useState, useEffect } from "react";
 import styles from "../styles/scss/write.module.scss";
-import tb from "../styles/scss/bar.module.scss";
-import { Link, useNavigate, useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import SearchModal from "../components/SearchModal";
 import { BsPlusLg } from "react-icons/bs";
 import { Book } from "../types/types";
 import ReviewBookInfo from "../components/ReviewBookInfo";
 import { getDate } from "../utils/getDate";
 import api from "../api/api";
+import TopBar from "../components/TopBar";
 
 export default function Write() {
   const navigate = useNavigate();
   const [modal, setModal] = useState(false);
   const [searchParams] = useSearchParams();
   const mode = searchParams.get("mode");
-  const id = searchParams.get("logNo");  
-  
+  const id = searchParams.get("logNo");
+
   const textareaRef = useRef<HTMLTextAreaElement | null>(null);
   const [text, setText] = useState("");
   const [book, setBook] = useState<Book | null>(null);
   const [rating, setRating] = useState(3);
   const today = new Date();
   const [date, setDate] = useState(getDate(today));
-  
+
   const token = localStorage.getItem("token");
 
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const onSubmit = (opt: "save" | "upload") => {
+    console.log(opt);
     if (!book) window.alert("후기를 작성할 책을 선택해주세요!");
     else if (text === "") window.alert("후기 내용을 입력해주세요!");
     else {
-      if (mode === "new") api
-        .post(
-          `/api/review/new`,
-          { book, rating, text, date: today },
-          {
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: `Bearer ${token}`,
-            },
-          }
-        )
-        .then(() => navigate("/"));
-      else api
-        .put(
-          `/api/review/${id}`,
-          { rating, text },
-          {
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: `Bearer ${token}`,
-            },
-          }
-        )
-        .then(() => navigate(`/review/detail/${id}`));
+      if (mode === "new") // 새 후기 저장
+        api
+          .post(
+            `/api/review/new`,
+            { book, rating, text, date: today, status: opt },
+            {
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          )
+          .then(() => navigate("/"));
+      else // 후기 수정
+        api
+          .put(
+            `/api/review/${id}`,
+            { rating, text },
+            {
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          )
+          .then(() => navigate(`/review/detail/${id}`));
     }
+  };
+
+  const showSavedReviews = () => {
+    console.log("임시저장 글 리스트 모달 띄우기");
   };
 
   const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -92,25 +98,7 @@ export default function Write() {
   return (
     <>
       {modal && <SearchModal setModal={setModal} setBook={setBook} />}
-      <div className={tb.wrapper}>
-        <div className={`${tb.topbar} ${tb.write}`}>
-          <ul>
-            <li className={tb.logo}>
-              <Link to="/">북북</Link>
-            </li>
-            <div className={tb.right}>
-              <li>
-                <span>저장</span>
-              </li>
-              <li>
-                <button type="submit" form="review">
-                  완료
-                </button>
-              </li>
-            </div>
-          </ul>
-        </div>
-      </div>
+      <TopBar write={{ mode: "new", savedCount: 1, onClick: onSubmit, onNumClick: showSavedReviews }} />
       <div className="wrapper">
         <div className={styles.wrapper}>
           <div className={styles.content}>
@@ -122,7 +110,7 @@ export default function Write() {
             ) : (
               <ReviewBookInfo book={book} setBook={setBook} rating={rating} setRating={setRating} isEdit={mode === "edit"} />
             )}
-            <form id="review" onSubmit={onSubmit} method="POST">
+            <form>
               <div className={styles.date}>{date}</div>
               <textarea ref={textareaRef} value={text} onChange={onChange} placeholder="내용을 입력하세요" autoFocus className={styles.textarea} />
             </form>
