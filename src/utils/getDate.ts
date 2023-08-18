e47d328e134137b183af905ea5247d2be0c50565
mihee <s2809h29@gmail.com>
2023-08-18 16:20:27 +0900

    feat: 날짜 포맷 추가

--- src/utils/getDate.ts
+++ src/utils/getDate.ts
@@ -1,7 +1,8 @@
-export const getDate = (date:Date) => {
+export const getDate = (date: Date, opt?:string) => {
   const year = date.getFullYear();
   const month = date.getMonth() + 1;
   const day = date.getDate();
+  if (opt === "-") return `${year}-${month}-${day}`;
   const weekday = ["일", "월", "화", "수", "목", "금", "토"];
   const week = weekday[date.getDay()];
   const today = `${year}년 ${month}월 ${day}일 ${week}요일`;
