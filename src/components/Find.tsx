85300e3f30104b7b6bebe7b505be98c764c8b696
mihee <s2809h29@gmail.com>
2023-08-28 22:11:23 +0900

    design: 비밀번호 찾기 UI 수정, auth logo inline-flex 적용
    auth.module.scss
    - logo와 label 사이 margin collapsing를 막기 위해 inline-flex 적용

--- src/components/Find.tsx
+++ src/components/Find.tsx
@@ -1,4 +1,4 @@
-import { useLocation, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import styles from "../styles/scss/auth.module.scss";
 
 export default function Find() {
@@ -7,21 +7,13 @@ export default function Find() {
   return (
     <div className={styles.wrapper}>
       <div className={styles.content}>
-        <a href="/">북북</a>
-        {pathname === "/find_id" && (
-          <>
-            <h4 className={styles.label}>등록된 이메일로 찾기</h4>
-            <form>
-              <input className={styles.input} required type="email" placeholder="이메일" />
-              <input className={styles.submit} type="submit" value="아이디 찾기" />
-            </form>
-          </>
-        )}
+        <div className={styles.logo}>
+          <Link to="/">북북</Link>
+        </div>
         {pathname === "/find_password" && (
           <>
-            <h4 className={styles.label}>계정 정보를 입력해주세요.</h4>
+            <h4 className={styles["label-sm"]}>계정 이메일을 입력해주세요.</h4>
             <form onSubmit={() => navigate("/reset_password")}>
-              <input className={styles.input} required placeholder="아이디" />
               <input className={styles.input} required type="email" placeholder="이메일" />
               <input className={styles.submit} type="submit" value="다음" />
             </form>
