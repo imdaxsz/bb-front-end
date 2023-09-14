22edce2ac589553a1514212689656202c7854a95
mihee <s2809h29@gmail.com>
2023-09-14 23:34:28 +0900

    fix: 이미 임시 저장된 후기를 다시 저장할 때 임시 저장 글 개수 바뀌지 않도록 함

--- src/hooks/useReview.ts
+++ src/hooks/useReview.ts
@@ -1,5 +1,5 @@
 import { useCallback, useState } from "react";
-import { Book } from "../types/types";
+import { Book, Review } from "../types/types";
 import { getDate } from "../utils/getDate";
 import api from "../api/api";
 import { useNavigate } from "react-router-dom";
@@ -7,7 +7,15 @@ import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../store/store";
 import { setCount } from "../store/savedReviewSlice";
 
-type addReviewFunType = (book: Book | null, rating: number, date: Date, text: string, opt: "save" | "upload", token: string | null) => Promise<any>;
+type addReviewFunType = (
+  book: Book | null,
+  rating: number,
+  date: Date,
+  text: string,
+  opt: "save" | "upload",
+  token: string | null,
+  savedReviews?: Review[]
+) => Promise<any>;
 
 export default function useReview() {
   const [text, setText] = useState("");
@@ -52,8 +60,16 @@ export default function useReview() {
     if (res.status === 200) navigate(`/review/detail/${id}`);
   };
 
-  const addReview: addReviewFunType = async (book, rating, date, text, opt, token) => {
+  const addReview: addReviewFunType = async (book, rating, date, text, opt, token, savedReviews) => {
+    setLoading(true);
     let reviewId = "";
+    let isSavedReview = false; // 이미 임시저장되어 있는 리뷰인가
+
+    if (savedReviews && savedReviews.length > 0) {
+      const savedIsbns = savedReviews.map((r) => r.book.isbn);
+      if (book && savedIsbns.includes(book.isbn)) isSavedReview = true;
+    }
+
     const res = await api.post(
       `/api/review/new`,
       { book, rating, date, text, status: opt },
@@ -64,11 +80,12 @@ export default function useReview() {
         },
       }
     );
+    setLoading(false);
     if (res.status === 200) {
       if (opt === "save") {
         // 임시 저장
         window.alert("저장 완료");
-        dispatch(setCount(savedCount + 1));
+        if (!isSavedReview) dispatch(setCount(savedCount + 1)); // 임시 저장에 없는 후기일 경우에만 개수 증가
       } else reviewId = res.data;
     } else {
       throw new Error("Failed to save review");
