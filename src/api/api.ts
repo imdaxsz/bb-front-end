94a6d370db67efeaacabd864b0feb3283e5501c4
mihee <s2809h29@gmail.com>
2023-09-19 18:16:23 +0900

    fix: api timeout
    - 이메일 인증 요청에 3초 이상의 시간이 수행되기 때문에 오류가 발생 > axios timeout 일시 제거

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,7 +2,6 @@ import axios, { AxiosError } from "axios";
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_ROOT,
-  timeout: 3 * 1000,
 });
 
 const isAxiosError = (error: unknown) => {
