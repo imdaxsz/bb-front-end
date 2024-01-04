1a741297b3e6f4529f469ea2b4308c388a833678
mihee <s2809h29@gmail.com>
2024-01-04 23:58:55 +0900

    feat: axios interceptor 추가

---
+++ src/lib/api/index.ts
@@ -0,0 +1,60 @@
+import axios from "axios";
+
+import { ApiError, BaseError } from "lib/error";
+import { store } from "store";
+
+const instance = axios.create({
+  baseURL: process.env.REACT_APP_API_ROOT,
+  timeout: 5 * 1000,
+});
+
+instance.interceptors.request.use((config) => {
+  const token = store.getState().auth.token;
+  if (token) config.headers["authorization"] = `Bearer ${token}`;
+  return config;
+});
+
+instance.interceptors.response.use(
+  (response) => {
+    return response.data;
+  },
+  (error) => {
+    if (error.response?.status === 401) {
+      console.log(error, "unauthorized");
+      // TODO: access token 제거
+    }
+    if (error.response?.data) {
+      const { message, status } = error.response.data;
+      return Promise.reject(new ApiError(message, status));
+    }
+
+    if (error.message.startsWith("timeout")) {
+      return Promise.reject(new BaseError("Timeout Error", "Network timeout"));
+    }
+
+    return Promise.reject(new BaseError("Unknown Error", error.message));
+  },
+);
+
+export function get<T>(...args: Parameters<typeof instance.get>) {
+  return instance.get<T, T>(...args);
+}
+export function post<T>(...args: Parameters<typeof instance.post>) {
+  return instance.post<T, T>(...args);
+}
+export function put<T>(...args: Parameters<typeof instance.put>) {
+  return instance.put<T, T>(...args);
+}
+export function patch<T>(...args: Parameters<typeof instance.patch>) {
+  return instance.patch<T, T>(...args);
+}
+export function del<T>(...args: Parameters<typeof instance.delete>) {
+  return instance.delete<T, T>(...args);
+}
+export default {
+  get,
+  post,
+  put,
+  patch,
+  delete: del,
+};
