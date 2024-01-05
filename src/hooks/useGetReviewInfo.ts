5fafa9eff38638e977f2108b6ced50cc9271c46d
mihee <s2809h29@gmail.com>
2024-01-05 14:35:55 +0900

    refactor: 리뷰 목록 조회, 리뷰 상세 조회 리팩토링
    - axios interceptor 및 응답 타입 추가된 api 적용

--- src/hooks/useGetReviewInfo.ts
+++ src/hooks/useGetReviewInfo.ts
@@ -1,37 +1,26 @@
 import { useCallback, useState } from "react";
 
-import api from "api";
+import { getReview } from "api/ReviewApi";
 import { Review } from "types";
 import { getDate } from "utils/getDate";
-
-import useSignOut from "./useSignout";
 
 export default function useGetReviewInfo() {
   const [review, setReview] = useState<Review | null>(null);
   const [date, setDate] = useState("");
   const [loading, setLoading] = useState(false);
 
-  const { signOut } = useSignOut();
-
   // 후기 상세 조회
-  const getReviewDetailInfo = useCallback(
-    async (id: string, token: string | null) => {
-      setLoading(true);
-      const res = await api.get(`/api/review/detail/${id}`, {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      if (res.status === 200) {
-        setReview(res.data);
-        setDate(getDate(new Date(res.data.date)));
-      }
-      if (res.status === 403) signOut();
-      setLoading(false);
-    },
-    [signOut],
-  );
+  const getReviewDetailInfo = useCallback(async (id: string) => {
+    setLoading(true);
+    try {
+      const res = await getReview(id);
+      setReview(res);
+      setDate(getDate(new Date(res.date)));
+    } catch (error) {
+      console.log(error);
+    }
+    setLoading(false);
+  }, []);
 
   return { loading, review, date, getReviewDetailInfo };
 }
--- src/hooks/useGetReviews.ts
+++ src/hooks/useGetReviews.ts
@@ -1,41 +1,21 @@
 import { useState, useCallback } from "react";
 
-import api, { isAxiosError, AxiosError } from "api";
+import { getReviews } from "api/ReviewApi";
 import { Review } from "types";
-
-import useSignOut from "./useSignout";
 
 export default function useGetReviews() {
   const [reviews, setReviews] = useState<Review[]>([]);
   const [loading, setLoading] = useState(true);
 
-  const { signOut } = useSignOut();
-
-  const getUserReviews = useCallback(
-    async (sort: string | null, token: string | null) => {
-      let url = `/api/review/list`;
-      if (sort === "date_asc") url = `/api/review/list?sort=date_asc`;
-      else if (sort === "title") url = `/api/review/list?sort=title`;
-      try {
-        const res = await api.get(url, {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-          },
-        });
-        setLoading(false);
-        if (res.status === 200) setReviews(res.data);
-      } catch (error) {
-        if (isAxiosError(error)) {
-          const axiosError = error as AxiosError;
-          if (axiosError.response && axiosError.response.status === 403)
-            signOut();
-        }
-        setLoading(false);
-      }
-    },
-    [signOut],
-  );
+  const getUserReviews = useCallback(async (sort: string | null) => {
+    try {
+      const res = await getReviews(sort);
+      setReviews(res);
+    } catch (error) {
+      console.log(error);
+    }
+    setLoading(false);
+  }, []);
 
   return { loading, setLoading, reviews, getUserReviews };
 }
