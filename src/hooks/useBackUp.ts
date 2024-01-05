110448ddc52a929b1a2701d1ca041f830615378f
mihee <s2809h29@gmail.com>
2024-01-05 14:32:55 +0900

    refactor: 후기 백업 리팩토링
    - axios interceptor 및 응답 타입 추가된 api 적용

--- src/hooks/useBackUp.ts
+++ src/hooks/useBackUp.ts
@@ -1,36 +1,19 @@
-import api, { isAxiosError, AxiosError } from "api";
-
-import useSignOut from "./useSignout";
+import { backUpReviews } from "api/ReviewApi";
 
 export default function useBackUp() {
-  const { signOut } = useSignOut();
-
-  const backUp = async (token: string | null) => {
+  const backUp = async () => {
     try {
-      const res = await api.get(`/api/review/backup`, {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      if (res.status === 200) {
-        const backupData = JSON.stringify(res.data, null, 2);
+      const res = await backUpReviews();
+      const backupData = JSON.stringify(res, null, 2);
+      const blob = new Blob([backupData], { type: "text/plain" });
+      const url = URL.createObjectURL(blob);
+      const link = document.createElement("a");
+      link.href = url;
+      link.download = "backup.txt";
+      link.click();
 
-        const blob = new Blob([backupData], { type: "text/plain" });
-        const url = URL.createObjectURL(blob);
-        const link = document.createElement("a");
-        link.href = url;
-        link.download = "backup.txt";
-        link.click();
-
-        URL.revokeObjectURL(url);
-      }
+      URL.revokeObjectURL(url);
     } catch (error) {
-      if (isAxiosError(error)) {
-        const axiosError = error as AxiosError;
-        if (axiosError.response && axiosError.response.status === 403)
-          signOut();
-      }
       console.error("Error downloading backup:", error);
     }
   };
