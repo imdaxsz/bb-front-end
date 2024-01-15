2cf6264c21dc56c77657c3b3604b3579d26e0a95
mihee <s2809h29@gmail.com>
2024-01-15 10:49:42 +0900

    feat: 후기 백업 요청 시 작성한 후기가 없을 경우 alert 추가

--- src/hooks/useBackUp.ts
+++ src/hooks/useBackUp.ts
@@ -5,6 +5,10 @@ export default function useBackUp() {
   const backUp = async () => {
     try {
       const res = await backUpReviews();
+      if (res.length === 0) {
+        window.alert("작성한 후기가 없습니다!");
+        return;
+      }
       const backupData = JSON.stringify(res, null, 2);
       const blob = new Blob([backupData], { type: "text/plain" });
       const url = URL.createObjectURL(blob);
