7c160726cfda7d4366ccb082fa58c85a5fbccd48
mihee <s2809h29@gmail.com>
2024-01-16 16:41:25 +0900

    refactor: 회원탈퇴 로직 hook으로 분리

--- src/hooks/useLeave.ts
+++ src/hooks/useLeave.ts
@@ -1,20 +1,32 @@
+import { debounce } from "lodash";
 import { useState } from "react";
 
 import { deleteAccount as request } from "api/UserApi";
 import { ApiError, handleUnauthorizated } from "lib/error";
 
-import useSignout from "./useSignout";
+import useSignOut from "./useSignOut";
 
 export default function useLeave() {
-  const { signout } = useSignout();
-  const [loading, setLoading] = useState(false);
+  const { signOut } = useSignOut();
+  const [isLoading, setIsLoading] = useState(false);
+  const [password, setPassword] = useState("");
+  const [agree, setAgree] = useState(false);
+
+  const onChangePw = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const onChangeCheck = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.checked) setAgree(true);
+    else setAgree(false);
+  };
 
   const deleteAccount = async (password: string) => {
-    setLoading(true);
+    setIsLoading(true);
     try {
       await request(password);
       window.alert("탈퇴 완료되었습니다.");
-      signout("/");
+      signOut("/");
     } catch (error) {
       if (error instanceof ApiError) {
         handleUnauthorizated(error, "alert");
@@ -22,8 +34,25 @@ export default function useLeave() {
       }
       console.log(error);
     }
-    setLoading(false);
+    setIsLoading(false);
   };
 
-  return { loading, deleteAccount };
+  const handleForm = debounce(async () => {
+    if (agree) {
+      await deleteAccount(password);
+    } else window.alert("회원 탈퇴 동의를 체크해 주세요.");
+  }, 300);
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    await handleForm();
+  };
+
+  return {
+    leaveInProgress: isLoading,
+    password,
+    onChangePw,
+    onChangeCheck,
+    onSubmit,
+  };
 }
