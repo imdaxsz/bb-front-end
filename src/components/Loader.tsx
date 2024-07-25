c6e2938d00bc091d90a9c0eb4c3d94f1f5c006ba
mihee <s2809h29@gmail.com>
2024-07-25 15:47:57 +0900

    Next.js migration 준비 #19

--- src/components/Loader.tsx
+++
@@ -1,7 +0,0 @@
-import { createPortal } from "react-dom";
-import { BeatLoader } from "react-spinners";
-
-export default function Loader() {
-  const el = document.getElementById("loader");
-  return el && createPortal(<BeatLoader color="#5e8b7e" />, el);
-}
--- src/utils/checkPassword.ts
+++
@@ -1,4 +0,0 @@
-export const checkPassword = (password: string) => {
-  const passwordReg = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,16}$/;
-  return passwordReg.test(password);
-};
