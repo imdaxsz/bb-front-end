10a2cc13f65a694400b64695f44e79243c0a53b3
mihee <s2809h29@gmail.com>
2024-07-31 18:31:42 +0900

    feat: route config 추가

---
+++ app/config/index.ts
@@ -0,0 +1,23 @@
+export const hasNoTopbarRoutes = [
+  '/signin',
+  '/signup',
+  '/auth/google',
+  '/find_password',
+  '/write',
+]
+
+export const privateRoutes = [
+  '/my',
+  '/review',
+  '/write',
+  '/leave',
+  '/search/review',
+  '/search/likes',
+]
+
+export const guestRoutes = [
+  '/signin',
+  '/signup',
+  '/auth/google',
+  '/find_password',
+]
