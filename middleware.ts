cd2e574eefb632f8f8c62f17d9737e2ad764ed9a
mihee <s2809h29@gmail.com>
2024-07-31 18:42:59 +0900

    refactor: 라우트 코드 분리

--- middleware.ts
+++ middleware.ts
@@ -1,28 +1,18 @@
+import { privateRoutes, guestRoutes } from '@/config'
 import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-
-const privateRoutes = [
-  '/my',
-  '/review',
-  '/write',
-  '/leave',
-  '/search/review',
-  '/search/likes',
-]
-
-const guestRoutes = ['/signin', '/signup', '/auth/google', '/find_password']
 
 export function middleware(request: NextRequest) {
   const token = cookies().get('token')
   const currentPath = request.nextUrl.pathname
 
-  const isCurrentPathPrivateRoute = privateRoutes.some(
-    (route) => currentPath === route || currentPath.startsWith(route),
+  const isCurrentPathPrivateRoute = privateRoutes.some((route) =>
+    currentPath.startsWith(route),
   )
 
-  const isCurrentPathGuestRoute = guestRoutes.some(
-    (route) => currentPath === route || currentPath.startsWith(route),
+  const isCurrentPathGuestRoute = guestRoutes.some((route) =>
+    currentPath.startsWith(route),
   )
 
   if (!token && isCurrentPathPrivateRoute) {
