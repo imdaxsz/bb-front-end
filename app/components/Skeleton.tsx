4e3780dee60bb51cea1c320ca35060f0b141f8c7
mihee <s2809h29@gmail.com>
2024-08-13 22:14:39 +0900

    feat: Skeleton 컴포넌트 구현 #25

---
+++ app/components/Skeleton.tsx
@@ -0,0 +1,25 @@
+import styles from '@/styles/skeleton.module.scss'
+
+interface SkeletonProps {
+  w: number
+  h: number
+  radius?: number
+  wUnit?: string
+  style?: React.CSSProperties
+}
+
+export default function Skeleton({
+  w,
+  h,
+  wUnit = 'px',
+  radius = 8,
+  style,
+}: SkeletonProps) {
+  const size: React.CSSProperties = {
+    width: `${w}${wUnit}`,
+    height: `${h}px`,
+    borderRadius: `${radius}px`,
+  }
+
+  return <div className={styles.container} style={{ ...size, ...style }} />
+}
