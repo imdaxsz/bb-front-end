e51dc452aac95eec093c818dfb60cfc6f741bb02
mihee <s2809h29@gmail.com>
2024-08-14 18:28:03 +0900

    fix: Pagination 버그 수정
    - Maximum Update Depth Exceeded 오류
    - 이동할 URL 설정을 위한 useEffect 제거
    - URLSearchParams로 URL 설정하도록 변경

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -4,6 +4,7 @@ import { useState, useEffect, Fragment } from 'react'
 import Link from 'next/link'
 import { v4 as uuidv4 } from 'uuid'
 import styles from '@/styles/pagination.module.scss'
+import { useSearchParams, usePathname } from 'next/navigation'
 
 interface Props {
   totalItems: number
@@ -18,20 +19,21 @@ export default function Pagination({
   pageCount,
   currentPage,
 }: Props) {
-  const [url, setUrl] = useState('')
   const [start, setStart] = useState(1)
   const totalPages = Math.ceil(totalItems / itemCountPerPage)
   const noPrev = start === 1
   const noNext = start + pageCount - 1 >= totalPages
 
-  useEffect(() => {
-    const currentUrl = window.location.href
-    if (currentUrl.includes('query') && url.includes('page'))
-      setUrl(currentUrl.split('page')[0])
-    else if (currentUrl.includes('query')) setUrl(`${currentUrl}&`)
-    else setUrl(`${currentUrl.split('?')[0]}?`)
-  }, [url])
+  const searchParams = useSearchParams()
+  const pathname = usePathname()
 
+  const getUrl = (page: number) => {
+    const params = new URLSearchParams(searchParams.toString())
+    params.set('page', page.toString())
+    return `${pathname}?${params.toString()}`
+  }
+
+  // 현재 보여줄 페이지 숫자 목록
   useEffect(() => {
     if (currentPage >= start + pageCount) setStart((prev) => prev + pageCount)
     if (currentPage < start) setStart((prev) => prev - pageCount)
@@ -43,7 +45,7 @@ export default function Pagination({
     <div className={styles.wrapper}>
       <ul>
         <li className={`${styles.move} ${noPrev && styles.invisible}`}>
-          <Link href={`${url}page=${start - 1}`}>이전</Link>
+          <Link href={getUrl(start - 1)}>이전</Link>
         </li>
         {[...Array(pageCount)].map((_, i) => (
           <Fragment key={uuidv4()}>
@@ -53,7 +55,7 @@ export default function Pagination({
                   className={`${styles.page} ${
                     currentPage === start + i && styles.active
                   }`}
-                  href={`${url}page=${start + i}`}
+                  href={getUrl(start + i)}
                 >
                   {start + i}
                 </Link>
@@ -62,7 +64,7 @@ export default function Pagination({
           </Fragment>
         ))}
         <li className={`${styles.move} ${noNext && styles.invisible}`}>
-          <Link href={`${url}page=${start + pageCount}`}>다음</Link>
+          <Link href={getUrl(start + pageCount)}>다음</Link>
         </li>
       </ul>
     </div>
