8521a270e02c4c31a447e71f0e41335ed657a29c
mihee <s2809h29@gmail.com>
2024-08-16 21:13:33 +0900

    chore: pwa 세팅 #26

--- next.config.js
+++ next.config.js
@@ -20,4 +20,11 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig
+const withPWA = require('@ducanh2912/next-pwa').default({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+})
+
+module.exports = withPWA(nextConfig)
