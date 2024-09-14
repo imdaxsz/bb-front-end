f220b794676658af71a2e2c75078d2995889abad
mihee <s2809h29@gmail.com>
2024-09-14 22:16:59 +0900

    feat: JSON-LD 추가 #20

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-danger */
 import type { Metadata, Viewport } from 'next'
 import '@/styles/global.scss'
 import RenderTopBar from './components/RenderTopBar'
@@ -10,6 +11,20 @@ export const metadata: Metadata = baseMetadata
 
 export const viewport: Viewport = {
   viewportFit: 'cover',
+}
+
+const jsonLd = {
+  '@context': 'https://schema.org',
+  '@type': 'WebSite',
+  name: '북북',
+  description: baseMetadata.description,
+  url: 'https://bookbook-phi.vercel.app',
+  potentialAction: {
+    '@type': 'SearchAction',
+    target:
+      'https://bookbook-phi.vercel.app/search/book?query={search_term_string}',
+    'query-input': 'required name=search_term_string',
+  },
 }
 
 export default function RootLayout({
@@ -26,6 +41,13 @@ export default function RootLayout({
           <main>{children}</main>
           <div id="loader" />
         </QueryClientProviders>
+        <script
+          type="application/ld+json"
+          suppressHydrationWarning
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify(jsonLd),
+          }}
+        />
       </body>
     </html>
   )
